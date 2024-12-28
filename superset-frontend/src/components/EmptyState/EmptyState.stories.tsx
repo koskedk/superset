4e2bdeaa3d8e92cbcb1c033bb0ca693b0052bce7
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-12-28 15:17:25 -0800

    fix empty states svg for theming

--- superset-frontend/src/components/EmptyState/EmptyState.stories.tsx
+++ superset-frontend/src/components/EmptyState/EmptyState.stories.tsx
@@ -11,62 +11,116 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
+import React from 'react';
+import { Card, Row, Col } from 'antd';
+
 import EmptyImage from 'src/assets/images/empty.svg';
 import ChartImage from 'src/assets/images/chart.svg';
 import FilterImage from 'src/assets/images/filter.svg';
-import { EmptyStateBig, EmptyStateMedium, EmptyStateSmall } from '.';
+import EmptyChartsImage from 'src/assets/images/empty-charts.svg';
+import EmptyDashboardImage from 'src/assets/images/empty-dashboard.svg';
+import UnionImage from 'src/assets/images/union.svg';
+import EmptyQueriesImage from 'src/assets/images/empty-queries.svg';
+
+import { EmptyStateSmall, EmptyStateMedium, EmptyStateBig } from './';
 
 export default {
-  title: 'Empty state',
+  title: 'Empty State Gallery',
   component: EmptyStateMedium,
+  argTypes: {
+    containerColor: {
+      control: 'color',
+      description: 'Background color of the container',
+      defaultValue: 'white',
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'big'],
+      defaultValue: 'medium',
+      description: 'Size of the Empty State components',
+    },
+  },
 };
 
-export const SmallEmptyState = () => (
-  <EmptyStateSmall
-    image={<FilterImage />}
-    title="Small empty state"
-    description="This is an example of a small empty state"
-  />
-);
+const emptyStates = [
+  {
+    title: 'Empty State',
+    description: 'This is the default empty state.',
+    image: <EmptyImage />,
+  },
+  {
+    title: 'Chart Empty State',
+    description: 'No charts available at the moment.',
+    image: <ChartImage />,
+  },
+  {
+    title: 'Filter Empty State',
+    description: 'No filters available at the moment.',
+    image: <FilterImage />,
+  },
+  {
+    title: 'Empty Charts',
+    description: 'No charts available.',
+    image: <EmptyChartsImage />,
+  },
+  {
+    title: 'Empty Dashboard',
+    description: 'No dashboards available.',
+    image: <EmptyDashboardImage />,
+  },
+  {
+    title: 'Recents',
+    description: 'No recent items found.',
+    image: <UnionImage />,
+  },
+  {
+    title: 'Empty Queries',
+    description: 'No queries saved yet.',
+    image: <EmptyQueriesImage />,
+  },
+];
 
-export const MediumEmptyState = () => (
-  <EmptyStateMedium
-    image={<ChartImage />}
-    title="Medium empty state"
-    description="This is an example of a medium empty state"
-  />
-);
+export const Gallery = ({ containerColor, size }) => {
+  const getComponentBySize = () => {
+    switch (size) {
+      case 'small':
+        return EmptyStateSmall;
+      case 'big':
+        return EmptyStateBig;
+      default:
+        return EmptyStateMedium;
+    }
+  };
 
-export const MediumEmptyStateWithButton = () => (
-  <EmptyStateMedium
-    image={<EmptyImage />}
-    title="Medium empty state"
-    description="This is an example of a medium empty state with a button"
-    buttonAction={() => {}}
-    buttonText="Click!"
-  />
-);
+  const SelectedEmptyState = getComponentBySize();
 
-export const BigEmptyState = () => (
-  <EmptyStateBig
-    image={<EmptyImage />}
-    title="Big empty state"
-    description="This is an example of a big empty state"
-  />
-);
+  return (
+    <Row gutter={[16, 16]}>
+      {emptyStates.map((state, index) => (
+        <Col key={index} xs={24} sm={12} md={8} lg={6}>
+          <Card title={state.title} hoverable style={{ textAlign: 'center' }}>
+            <div style={{ color: containerColor, padding: 16 }}>
+              <SelectedEmptyState
+                image={state.image}
+                title={state.title}
+                description={state.description}
+              />
+            </div>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
-export const BigEmptyStateWithButton = () => (
-  <EmptyStateBig
-    image={<ChartImage />}
-    title="Big empty state"
-    description="This is an example of a big empty state with a button"
-    buttonText="Click!"
-    buttonAction={() => {}}
-  />
-);
+Gallery.args = {
+  containerColor: 'white', // Default background color
+  size: 'medium', // Default size
+};
